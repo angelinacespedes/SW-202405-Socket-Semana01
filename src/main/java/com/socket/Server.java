0e94c46a4af87d0a1b7a03743506020b9e79a48f
Angelina Cespedes <angelinacespedes@gmail.com>
2024-09-12 22:15:58 -0500

    Commit

--- src/main/java/com/socket/Server.java
+++ src/main/java/com/socket/Server.java
@@ -17,7 +17,6 @@ public class Server {
         try (ServerSocket serverSocket = new ServerSocket(PORT)) {
 
             System.out.println("1 > Server started on port " + PORT);
-
             while (true) {
                 System.out.println("2 > A la espera de un cliente");
                 clientSocket = serverSocket.accept();
